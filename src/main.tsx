0cd6e5a0204ef559c1f58c29aeb695b6244693cc
mily04 <mily040625@gmail.com>
2026-04-22 15:14:45 +0800

    Add AirChat project files

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2026 mily04
+ * This file is part of AirChat.
+ *
+ * Licensed under the GNU Affero General Public License, version 3 or later.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Commercial licensing is available from: mily040625@gmail.com
+ */
+
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
